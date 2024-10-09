f31c33c4c8284ab4813065a8e83329ee7b5fb1f4
jomatheux <jomatheux@gmail.com>
2024-10-09 20:28:29 -0300

    Desenvolvimento-Gerenciamento de Candidatos - Repositório - Parte 2

--- election-management/src/test/java/infrastructure/repositories/SQLCandidateRepositoryTest.java
+++ election-management/src/test/java/infrastructure/repositories/SQLCandidateRepositoryTest.java
@@ -2,9 +2,12 @@ package infrastructure.repositories;
 
 import domain.CandidateRepository;
 import domain.CandidateRepositoryTest;
+import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.AfterEach;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 @QuarkusTest
@@ -13,8 +16,19 @@ class SQLCandidateRepositoryTest extends CandidateRepositoryTest {
     @Inject
     SQLCandidateRepository repository;
 
+    @Inject
+    EntityManager entityManager;
+
     @Override
     public CandidateRepository repository() {
         return repository;
     }
+
+    @AfterEach
+    @TestTransaction
+    void tearDown() {
+        entityManager.createNativeQuery("TRUNCATE TABLE candidates").executeUpdate();
+    }
+
+
 }
